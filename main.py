a022b9e3e4654ed9828a055ddcf073f78dc0ed1b
Francisco_GP <91037607+PACKYTO23@users.noreply.github.com>
2023-12-22 17:19:06 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+# / # / # PROJECT OF DAY 15 # / # / #
+
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+profit = 0
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+# TODO: 1-. Ask "What would you like? (espresso/latte/cappuccino): "
+
+# TODO: 2-. If input is "report" show the amount of resources left (and money).
+
+# TODO: 3-. If input is "off" terminate the program.
+
+# TODO: 4-. If input is {coffee_choice} check for availability. If available TODO: 5-. Else TODO: 8-.
+
+# TODO: 5-. Say "Please insert coins." "How many quarters/dimes/nickles/pennies?: "
+
+# TODO: 6-. If {sum_coins} not enough for {coffee_choice} say "Sorry that's not enough money. Money refunded." TODO: 1-.
+
+# TODO: 7-. Say "Here is ${change} in change." "Here is your {coffee_choice} ☕️. Enjoy!" TODO: 1-.
+
+# TODO: 8-. Say "Sorry there is not enough {resource}." TODO: 1-.
